560613b090308c70db596e240db7b92ed3bc6cee
HighandLight <84646461+HighandLight@users.noreply.github.com>
2023-04-14 22:16:51 +0900

    Time: 1100 ms (5.01%), Space: 43.5 MB (30.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,18 +3,18 @@ class Solution {
         // String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z\\s]";
         String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z]";
 
-        s = s.replaceAll(match, "");
-        s = s.toLowerCase();
+        s = s.replaceAll(match, "");  //O(n)
+        s = s.toLowerCase();  //O(n)
         
         String n = "";
         
-        for (int i = s.length() - 1; i >= 0;i--) {
+        for (int i = s.length() - 1; i >= 0;i--) {  //O(n)
             n = n + s.charAt(i);
         }
         System.out.println(s);
         System.out.println(n);
         
-        boolean ans = (s.equals(n)) ? true : false;
+        boolean ans = (s.equals(n)) ? true : false;  //O(n)
         return ans;
     }
 }
