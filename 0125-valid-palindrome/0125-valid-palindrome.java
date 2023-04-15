cd7f0652772a242d889e6776817c4697d1d4ed79
HighandLight <84646461+HighandLight@users.noreply.github.com>
2023-04-16 03:09:55 +0900

    Time: 741 ms (12.12%), Space: 43.6 MB (30.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -14,7 +14,7 @@ class Solution {
 
 //         boolean ans = (s.equals(n)) ? true : false;  //O(n)
 //         return ans;
-        String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z]";
+        String match = "[^a-zA-Z0-9]"; // [] : 문자열, ^ : not, a-z : 소문자 전체, A-Z : 대문자 전체, 0-9: 숫자 전체 
 
         s = s.replaceAll(match, "");  //O(n)
         s = s.toLowerCase();  //O(n)
