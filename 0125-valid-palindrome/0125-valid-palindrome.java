14a30418316ce73c244835511a6e4ee131b871ba
HighandLight <84646461+HighandLight@users.noreply.github.com>
2023-04-16 03:03:56 +0900

    Time: 754 ms (5.87%), Space: 43.5 MB (35.33%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,33 @@
 class Solution {
     public boolean isPalindrome(String s) {
         // String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z\\s]";
+//         String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z]";
+
+//         s = s.replaceAll(match, "");  //O(n)
+//         s = s.toLowerCase();  //O(n)
+        
+//         String n = "";
+        
+//         for (int i = s.length() - 1; i >= 0;i--) {  //O(n)
+//             n = n + s.charAt(i);
+//         }
+
+//         boolean ans = (s.equals(n)) ? true : false;  //O(n)
+//         return ans;
         String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z]";
 
         s = s.replaceAll(match, "");  //O(n)
         s = s.toLowerCase();  //O(n)
+        int left = 0;
+        int right = s.length()-1;
         
-        String n = "";
-        
-        for (int i = s.length() - 1; i >= 0;i--) {  //O(n)
-            n = n + s.charAt(i);
+        while(left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left ++;
+            right --;
         }
-
-        boolean ans = (s.equals(n)) ? true : false;  //O(n)
-        return ans;
+        return true;
     }
 }
