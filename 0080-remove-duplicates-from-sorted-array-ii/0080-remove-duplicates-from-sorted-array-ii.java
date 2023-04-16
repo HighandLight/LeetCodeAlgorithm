aa99aaaafc4657c761b0ad10ab48b3dbb2039f92
HighandLight <84646461+HighandLight@users.noreply.github.com>
2023-04-16 10:55:16 +0900

    Time: 0 ms (100.00%), Space: 42.8 MB (45.28%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,23 +1,35 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int index = 1;
-        int count = 0;
+//         int index = 1;
+//         int count = 0;
         
-        for (int i =1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]) {
-                count++;
+//         for (int i =1;i<nums.length;i++){
+//             if(nums[i]==nums[i-1]) {
+//                 count++;
                 
-            }else{
-                count = 0;
+//             }else{
+//                 count = 0;
         
-            }
-            if(count <=1) {
-                nums[index]= nums[i];
-                index++;
+//             }
+//             if(count <=1) {
+//                 nums[index]= nums[i];
+//                 index++;
+                
+//             }
+//         }
+//         return index;
+        if(nums.length ==1) return 1;
+
+        int count = 2;
+
+        for(int i = 2;i< nums.length;i++) {
+            if(nums[count-2] < nums[i]) {
+                nums[count++] = nums[i];
                 
             }
         }
-        return index;
-        
+        return count;
     }
 }
+
+
