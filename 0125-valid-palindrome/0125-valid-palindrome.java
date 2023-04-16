c2dae90f11390f08c16a06539f442d73a3cc70fb
HighandLight <84646461+HighandLight@users.noreply.github.com>
2023-04-16 13:02:34 +0900

    Time: 3 ms (97.93%), Space: 43 MB (54.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,33 +1,39 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z\\s]";
-//         String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z]";
 
-//         s = s.replaceAll(match, "");  //O(n)
-//         s = s.toLowerCase();  //O(n)
+        int left = 0; int right = s.length() -1;
         
-//         String n = "";
-        
-//         for (int i = s.length() - 1; i >= 0;i--) {  //O(n)
-//             n = n + s.charAt(i);
-//         }
+        while(left < right) {
+            if(! Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+                continue;
+            }
 
-//         boolean ans = (s.equals(n)) ? true : false;  //O(n)
-//         return ans;
-        String match = "[^a-zA-Z0-9]"; // [] : 문자열, ^ : not, a-z : 소문자 전체, A-Z : 대문자 전체, 0-9: 숫자 전체 
-
-        s = s.replaceAll(match, "");  //O(n)
-        s = s.toLowerCase();  //O(n)
-        int left = 0;
-        int right = s.length()-1;
-        
-        while(left <= right) {
-            if (s.charAt(left) != s.charAt(right)) {
+            if(! Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
-            left ++;
-            right --;
-        }
+            left++;
+            right--;
+            }
         return true;
     }
 }
+
+/*
+    while (sleft.isLoD != True) {left++;}
+    while (sright.isLoD !=True) {right--;}
+    
+    if(sleft.LC != sright.LC) {
+        return false;
+    }
+    left++;
+    right--;
+    }
+    
+    return true
+    
+*/
