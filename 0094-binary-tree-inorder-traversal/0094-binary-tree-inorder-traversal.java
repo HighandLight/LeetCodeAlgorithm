34c904bc9708d782d3c692ee746c6a364be06670
HighandLight <84646461+HighandLight@users.noreply.github.com>
2023-05-05 21:39:01 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (7.30%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) { //basecase_기반 케이스
+        ArrayList<Integer> list = new ArrayList<>(); // 다양한 유틸리티, collectionframework 도입 이후 여러 유용한 자료 들어옴. 
+        // basecase
+        if(root == null) {
+            return list;
+        }
+        list.addAll(inorderTraversal(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal(root.right));
+        return list;
+    }
+}
+
+        
